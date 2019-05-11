2d5c05de8858b1c7cb7d6e2f69c1fa4cc0f5fe9c
Varun Sukheja <>
2019-05-11 14:52:29 +0530

    established web socket connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular7-live-stocks';
+  socketUrl = 'ws://stocks.mnet.website';
+  ws: WebSocket;
+
+  constructor() {
+    this.ws = new WebSocket(this.socketUrl);
+    this.ws.onmessage = function (evt) {
+      let received_msg = evt.data;
+      console.log("Message is received...", received_msg);
+    };
+  }
 }
